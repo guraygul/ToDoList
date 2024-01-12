fb51ac1cb6edbe3ca0ec56178ff2bb99782462c0
Güray Gül <guraygul59@gmail.com>
2024-01-12 22:04:56 +0300

    Added a keyboard recognizer

--- ToDoList/Controller/ModalViewController.swift
+++ ToDoList/Controller/ModalViewController.swift
@@ -13,11 +13,17 @@ class ModalViewController: UIViewController {
     
     @IBOutlet weak var addTasks: UIView!
     
+    @IBOutlet weak var inputViewBottom: NSLayoutConstraint!
+    
+    @IBOutlet weak var inputTask: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        modalPresentationStyle = .overCurrentContext
+        //modalPresentationStyle = .overCurrentContext
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
 }
@@ -29,4 +35,22 @@ extension ModalViewController{
     @IBAction func closeButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @objc private func adjustInputView(noti: Notification) {
+        guard let userInfo = noti.userInfo else { return }
+        guard let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        if noti.name == UIResponder.keyboardWillShowNotification {
+            let adjustmentHeight = keyboardFrame.height - view.safeAreaInsets.bottom
+            inputViewBottom.constant = adjustmentHeight
+        }
+        else {
+            inputViewBottom.constant = 0
+        }
+        //print("\(keyboardFrame)")
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        inputTask.resignFirstResponder()
+    }
+    
 }
